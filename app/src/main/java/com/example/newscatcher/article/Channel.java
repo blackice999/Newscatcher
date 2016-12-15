80d50b33ff6ab2691db06e855f12bf9e1d2913b7
blackice999 <adam596341@yahoo.com>
2016-12-15 18:59:36 +0200

    Delete commented code

--- app/src/main/java/com/example/newscatcher/article/Channel.java
+++ app/src/main/java/com/example/newscatcher/article/Channel.java
@@ -6,18 +6,7 @@ import java.util.ArrayList;
  * Created by W7 on 10.01.2016.
  */
 public class Channel {
-//    public static final String title = null;
-//    public static final String TITLE = new Channel().getTitle();
     private String title;
-
-//    public static final String TITLE;
-
-
-//    public Channel(String title) {
-////        Channel.title = title;
-////        this.title = title;
-//       this.title = title;
-//    }
 
     public String getTitle() {
         return title;
